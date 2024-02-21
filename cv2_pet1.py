7ab07166261a45350e4d501fb7ecd181d98bae95
Valentine <125474713+valntn1@users.noreply.github.com>
2024-02-21 19:48:55 +0200

    Update cv2_pet1.py

--- cv2_pet1.py
+++ cv2_pet1.py
@@ -1,21 +1,23 @@
 import cv2
-from tracker import *
+from cv2 import VideoCapture
+from tracker import * #tracker code file added
 
 # Create tracker object
 tracker = EuclideanDistTracker()
 
-cap = cv2.VideoCapture('/Users/valntn/Documents/Study/Datasets/video/highway.mov')
+cap: VideoCapture = cv2.VideoCapture('../../Documents/Study/Datasets/video/highway.mov')
 
 # Object detection from stable camera
-object_detector = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=60)
+object_detector = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=70)
 
-while cap.isOpened():
+while True:
     ret, frame = cap.read()
     height, width, _ = frame.shape
+
     # Extract region of interest
     roi = frame[220: 710, 650: 1270]
 
-    # ------------------------------------------------------------ 1. Object detection
+    # --------------- 1. Object detection
     mask = object_detector.apply(roi)
     _, mask = cv2.threshold(mask, 254, 255, cv2.THRESH_BINARY)
     contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -24,26 +26,24 @@ while cap.isOpened():
         # Calculate area and declutter
         area = cv2.contourArea(cnt)
         # cv2.drawContours(roi, [cnt], -1, (0, 255, 0), 2)
-        if area < 1200:
-            continue
-        x, y, w, h = cv2.boundingRect(cnt)
-        cv2.rectangle(roi, (x, y), (x + w, y + h), (0, 255, 0), 3)
-        detections.append([x, y, w, h])
-    print(detections)
+        if area > 1200:
+            x, y, w, h = cv2.boundingRect(cnt)
+            # cv2.rectangle(roi, (x, y), (x + w, y + h), (0, 255, 0), 3)
+            detections.append([x, y, w, h])
 
-    # ------------------------------------------------------------ 2. Object tracking
+    # --------------- 2. Object tracking
     boxes_ids = tracker.update(detections)
 
     for box_id in boxes_ids:
-        id: object
         x, y, w, h, id = box_id
         cv2.putText(roi, str(id), (x, y - 15), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), 2)
+        cv2.rectangle(roi, (x, y), (x + w, y + h), (0, 255, 0), 3)
 
     cv2.imshow('Frame', frame)
     # cv2.imshow('Mask', mask)
     # cv2.imshow('roi', roi)
-    key = cv2.waitKey(1000)
 
+    key = cv2.waitKey(30)
     if key == 27:
         break
 
